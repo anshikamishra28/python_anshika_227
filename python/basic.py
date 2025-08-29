ee33641f8b9647db78a5aa5075affe0275f77791
anshikamishra28 <28anshika0605@gmail.com>
2025-08-29 15:50:04 +0530

    learned basic datatypes

--- python/basic.py
+++ python/basic.py
@@ -3,3 +3,5 @@ print(a)
 id(a)
 print(id(a))
 print(type(a))
+b="""this is a multiline comment"""
+print(b)
---
+++ python/day2.py
@@ -0,0 +1,24 @@
+b="hello"
+c=b.replace("e","l")
+print(c)
+a="hello"
+b="world"
+c=a+"yes"+b
+d=print(c)
+age=10
+text=f"my age is {age}"
+print(text)
+def myfunction():
+    return True
+print(myfunction())
+a=[1,2,'hello',3,4]
+print(type(a))
+id(a)
+a=[1,2,3,'hello']
+print(a[1])
+a=[1,2,3,'world']
+b=[7,4,'hello']
+print(a+b)
+print(a*2)
+a="hello"
+a[2:4]
